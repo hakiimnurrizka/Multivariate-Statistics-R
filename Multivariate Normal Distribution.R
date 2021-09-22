a61976e7f6e740b5b10ca72043cc284605e1c473
hakiimnurrizka <hakiimnur.2017@gmail.com>
2021-09-22 11:31:53 +0700

    Multivariate Normal Distribution

---
+++ Multivariate Normal Distribution.R
@@ -0,0 +1,30 @@
+###multivariate normal distribution###
+library(mvtnorm)
+library(MVN)
+
+##simulating data from MN(mu,sigma)
+set.seed(100)
+mu = c(1,221,98)
+sigma = matrix(c(200,1,-2,1,1325,4,-2,4,1432), nrow = 3, ncol = 3, byrow = T)
+
+x = rmvnorm(100,mu , sigma,method=c("eigen", "svd", "chol"), pre0.9_9994 = FALSE)
+head(x)
+list_x = list(x[,1],x[,2], x[,3])
+
+makeProfilePlot(list_x, names= c("v1","v2", "v3"))
+
+##Multiv normality test
+#mardia
+mvn(iris, mvnTest = "mardia")
+#HZ
+mvn(iris, mvnTest = "hz")
+#Royston
+mvn(iris, mvnTest = "royston")
+#DH
+mvn(iris, mvnTest = "dh")
+#energy
+mvn(iris, mvnTest = "energy")
+
+#plot perspective n contour
+mvn(iris[,2:3], mvnTest = "hz", multivariatePlot = "persp")
+mvn(iris[,3:4], mvnTest = "mardia", multivariatePlot = "contour")
