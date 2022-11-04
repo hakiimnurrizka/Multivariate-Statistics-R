991867293559f988d4167467584afb817e66d6b6
hakiimnurrizka <hakiimnur.2017@gmail.com>
2022-11-04 15:41:42 +0700

    Update Testing mean vector.R

--- Testing mean vector.R
+++ Testing mean vector.R
@@ -15,6 +15,8 @@ library(gridExtra)
 library(tidyverse)
 library(DescTools)
 library(MVN)
+library(RVAideMemoire)
+library(BSDA)
 
 ##On testing mean vectors
 #at this point, most of us should have already been familiar on testing mean from 2 variables which is a univariate case.
@@ -28,8 +30,7 @@ library(MVN)
 #simulate the samples
 meanvect = c(13,5,1250)
 cov_m = matrix(c(124,123,47,
-
-                                  123,203,162,
+                123,203,162,
                  47,162,3257), ncol = 3)
 #define a function to sample from a MN dist
 set.seed(123)
@@ -41,6 +42,7 @@ gen.data = function(mu, sigma, n) {
 }
 gen.samples = gen.data(meanvect, cov_m, 300)
 colnames(gen.samples) = c("v1", "v2", "v3")
+view(gen.samples)
 plot3d(gen.samples)
 
 #Testing for one sample
@@ -50,24 +52,65 @@ n = dim(gen.samples)[1]
 mu.sam = apply(gen.samples, 2, mean)
 mu = c(10, 5, 1200)
 
-z2 = n*t(mu.sam-mu)%*%solve(cov_m)%*%(mu.sam-mu)
-p.z2 = dchisq(z2, 3) #Z2 is distributed as chisq(p), p : degree of freedom if null is true
+z.2 = n*t(mu.sam-mu)%*%solve(cov_m)%*%(mu.sam-mu)
+p.z2 = pchisq(z.2, 3, lower.tail = F) #Z2 is distributed as chisq(p), p : degree of freedom if null is true
 p.z2
 #We conclude from the pvalue above that there is not enough evidence for the null hypothesis 
 #mu* = mu = c(10,5,1200) to be accepted
+HotellingsT2Test(gen.samples, mu = mu, cov_m, test = "chi")#Using function from desctools package
 
-#Using hotellingT2 function from desctools package for unknown covariance matrix
-HotellingsT2Test(gen.samples, mu = mu, cov_m)
+#Testing mu for unknown covariance matrix
+t2 = n*t(mu.sam-mu)%*%solve(cov(gen.samples))%*%(mu.sam-mu)#T2 statistics, 
+f.eq = (297/(3*299))*t2
+HotellingsT2Test(gen.samples, mu = mu, cov(gen.samples), test = "f")
+
+##Side Note: For function HotellingsT2Test above, the statistics are actually the approximation distribution 
+##stated in the argument "test = ". That is for the test = "chi" the T.2 in the output is actually chi square
+##approximation, a similar logic follows for test = "f". Another note is that the scaled value of the statistics
+##is only applicable for 2 sample test, so for above example we can only use the p value.
 
 #assumption test
-mvn(gen.samples) #the result is expected since we draw the sample from a normal distributio
-mshapiro.test(t(gen.samples))
+mvn(gen.samples) #the result is expected since we draw the sample from a normal distribution
+#But there is an interesting finding, where the univariate normality of the 3rd variable is rejected
+mshapiro.test(gen.samples)#From library RVAideMemoire
 det(cov(gen.samples))
 chol(cov(gen.samples))
 
-#Compare the result with univariate case
+##Comparison the result with univariate case
 #H0 : (1) mu1 = 10; (2) mu2 = 5; (3) mu3 = 1200
-z.test(gen.samples[,1], sigma2 = 124, mu = 10)
-z.test(gen.samples[,2], sigma2 = 203, mu = 5)
-z.test(gen.samples[,3], sigma2 = 3257, mu = 1200)
-#it can be seen that the 2nd variable ztest results in the acceptance of null hypothesis
+
+#Known Variance using the covariance matrix from multivariate normal distribution above
+cov_m
+#1st variable
+z1 = (mean(gen.samples[,1])-10)/(124/sqrt(length(gen.samples[,1])))#Z statistics
+z1
+2*pnorm(z1, 0, 1, lower.tail = F)#P value for testing 2 way hypothesis with alternative h1 : mu1 =/= 10
+z.test(gen.samples[,1], sigma.x = 124, mu = 10)
+#2nd variable
+z2 = (mean(gen.samples[,2])-5)/(203/sqrt(length(gen.samples[,2])))
+z2
+2*pnorm(z2, 0, 1, lower.tail = F)#P value for testing 2 way hypothesis with alternative h1 : mu1 =/= 10
+#Rounding error, leads to pvalue > 1
+z.test(gen.samples[,2], sigma.x = 203, mu = 5)
+#3rd variable
+z3 = (mean(gen.samples[,3])-1200)/(3257/sqrt(length(gen.samples[,3])))
+z3
+2*pnorm(z3, 0, 1, lower.tail = F)
+z.test(gen.samples[,3], sigma.x = 3257, mu = 1200)[2]
+#All individual univariate tests have the same conclusion, that is acceptance of null hypothesis
+ztest.ind = data.frame(var1 = c(NA, NA), var2 = c(NA,NA), var3 = c(NA,NA))
+rownames(ztest.ind) = c("z stat", "2 way-p value")
+for(j in 1:3){
+    ztest.ind[1,j] = as.numeric(z.test(gen.samples[,j], sigma.x = cov_m[j,j], mu = mu[j])[1])
+    ztest.ind[2,j] = as.numeric(z.test(gen.samples[,j], sigma.x = cov_m[j,j], mu = mu[j])[2])
+}
+
+
+#Unknown variance
+p.val_test = data.frame(pv.test1 = NA, pv.test2 = NA, pv.test3 = NA)
+for(i in 1:3){
+  p.val_test[i] = as.numeric(t.test(gen.samples[,i], mu = mu[i])[3])
+}
+p.val_test
+#Using T test for individual testing of null hypothesis mu_sam[i] = mu[i], it is found that only 2nd variable
+#test that has its null hypothesis accepted.
